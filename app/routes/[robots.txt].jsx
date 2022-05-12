8ee3249c800e9bbf1e6cb7f6f9188ca7f8a0aa4d
Vaibhav Acharya <vaibhavacharya111@gmail.com>
2022-05-12 10:16:58 +0530

    try: fix unstyled content

--- app/routes/[robots.txt].jsx
+++
@@ -1,9 +0,0 @@
-export function loader() {
-  const content = `User-agent: *\nDisallow:\n\nSitemap: https://www.jobilist.com/sitemap.xml`;
-
-  return new Response(content, {
-    headers: {
-      "Content-Type": "text/plain",
-    },
-  });
-}
